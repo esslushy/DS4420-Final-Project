840e67d87d9bae33fefcc9a6b5a248fa011f4aad
Claire Schlesinger <esslushy@gmail.com>
2024-10-29 00:50:55 -0400

    allowed for entity update

--- src/visualize.py
+++ src/visualize.py
@@ -6,12 +6,14 @@ from functools import partial
 from Utils import load_module
 
 def draw_world(frame, world: World, ax):
+    world.update_entities_position()
     ax.set_xlim(0, world.width)
     ax.set_ylim(0, world.height)
     for entity in world.entities:
         ax.add_patch(plt.Circle((entity.position.x, entity.position.y), entity.radius, color="black"))
     ax.add_patch(plt.Circle((world.robot.position.x, world.robot.position.y), world.robot.radius, color="red"))
     ax.add_patch(plt.Circle((world.goal.position.x, world.goal.position.y), world.goal.radius, color="green"))
+    world.update_entities_velocity()
 
 def main(world: World):
     fig, ax = plt.subplots(figsize=(8,8), tight_layout=True)
