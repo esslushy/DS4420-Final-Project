a9b2dfb7946cd4e19e29ee9096e731f132e6a80e
Claire Schlesinger <esslushy@gmail.com>
2024-11-01 12:43:49 -0400

    messing with params

--- src/ProjectParameters.py
+++ src/ProjectParameters.py
@@ -4,11 +4,12 @@ STEP_SIZE=0.5
 MAX_ANGLE_CHANGE=math.pi/8 # Radians
 MAX_SPEED_CHANGE=0.1
 GAMMA = 0.9 # Reward decay
-BETA = 0.2 # Entropy factor
-BETA_DECAY = 0.01 # How fast Beta decays (encourage less exploration over time)
+BETA = 0.1 # Entropy factor
+BETA_DECAY = 0.05 # How fast Beta decays (encourage less exploration over time)
 # Reward
-DISTANCE_SCALE = 10000
-COLLISION_SCALE = 10
-TIME_SCALE = 1
-SUCCESS_REWARD = 10000
-SPEED_SCALE = 1 # Bonus provided for robot going faster 
+DISTANCE_SCALE = 100
+COLLISION_SCALE = 10000
+TIME_SCALE = 0.1
+SUCCESS_REWARD = 1000
+SPEED_SCALE = 0.1 # Bonus provided for robot going faster 
+
