a96d4d00176612429c8b7a8216cbbb3eb35f165d
Claire Schlesinger <esslushy@gmail.com>
2024-11-03 20:55:26 -0500

    running more experiments

--- src/ProjectParameters.py
+++ src/ProjectParameters.py
@@ -4,12 +4,12 @@ STEP_SIZE=0.5
 MAX_ANGLE_CHANGE=math.pi/8 # Radians
 MAX_SPEED_CHANGE=0.1
 GAMMA = 0.9 # Reward decay
-BETA = 0.1 # Entropy factor
-BETA_DECAY = 0.05 # How fast Beta decays (encourage less exploration over time)
+BETA = 10 # Entropy factor
+BETA_DECAY = 0.0001 # How fast Beta decays (encourage less exploration over time)
 # Reward
 DISTANCE_SCALE = 100
 COLLISION_SCALE = 10000
 TIME_SCALE = 0.1
-SUCCESS_REWARD = 1000
+SUCCESS_REWARD = 100000
 SPEED_SCALE = 0.1 # Bonus provided for robot going faster 
 
